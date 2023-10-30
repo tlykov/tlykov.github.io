5f7dc2dc02ffb68b9f4f509b22d67c3cd1e5951c
tlykov <tim_lykov@sfu.ca>
2023-10-29 22:32:07 -0700

    home page background

--- resume-app/src/pages/Home.tsx
+++ resume-app/src/pages/Home.tsx
@@ -1,8 +1,17 @@
 import React from "react";
+import pic from '../assets/extra/bg-picture-wide.png?url'
+import '../assets/styles/Home.css';
 
 function Home() {
     return (
-        <p>Home Page</p>
+        <div id="home-div">
+            <div className="left-sidebar"></div>
+            <div className="main-content">
+                <h1 id="subtitle">Software Developer</h1>
+                <img id="bg-pic" src={pic}></img>
+            </div>
+            <div className="right-sidebar"></div>
+        </div>
     );
 }
 
