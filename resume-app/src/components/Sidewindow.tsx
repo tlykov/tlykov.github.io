5fc1caaaf8381578003b97fc6d0d58b4f7c3524c
tlykov <tim_lykov@sfu.ca>
2023-11-04 18:56:51 -0700

    refactor skills menu

--- resume-app/src/components/Sidewindow.tsx
+++ resume-app/src/components/Sidewindow.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import '../assets/styles/Sidewindow.css';
 import Skill from './Skill';
+import { SkillList as skills } from '../utils/SkillList';
 
 function SideWindow() {
     const [isActive, setIsActive] = useState(false);
@@ -10,18 +11,9 @@ function SideWindow() {
         <button className="trigger" onClick={onClick}>
             <div className={`side-menu ${isActive ? 'active' : 'inactive'}`}>
                 <ul id="skill-list">
-                    <Skill key={0} text={"C++"} level={3} />
-                    <Skill key={1} text={"C"} level={2} />
-                    <Skill key={2} text={"Java"} level={2} />
-                    <Skill key={3} text={"Python"} level={2} />
-                    <Skill key={4} text={"Javascript"} level={3} />
-                    <Skill key={5} text={"Typescript"} level={3} />
-                    <Skill key={6} text={"HTML"}level={3} />
-                    <Skill key={7} text={"CSS"} level={3} />
-                    <Skill key={8} text={"Node.js"} level={3} />
-                    <Skill key={9} text={"Angular"} level={3} />
-                    <Skill key={10} text={"React"} level={2} />
-                    <Skill key={11} text={"x86"} level={1} />
+                    {skills.map((el, i) =>
+                        <Skill key={i} text={el.name} level={el.level}/>
+                    )}
                 </ul>
             </div>
         
