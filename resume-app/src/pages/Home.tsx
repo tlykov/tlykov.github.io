09208f9fa7084c9398718e5d845ee5f549b7101c
tlykov <tim_lykov@sfu.ca>
2023-11-04 21:04:57 -0700

    fix production file download

--- resume-app/src/pages/Home.tsx
+++ resume-app/src/pages/Home.tsx
@@ -1,5 +1,6 @@
 import { motion } from "framer-motion";
 import pic from '../assets/extra/bg-picture-wide.png?url'
+import file from '../assets/files/resume.pdf?url'
 import '../assets/styles/Home.css';
 import { SideWindow } from "../components";
 
@@ -9,7 +10,7 @@ function Home() {
             <div className="left-sidebar"></div>
             <div className="main-content">
                 <h1 id="subtitle">Software Developer</h1>
-                <motion.a className="download-btn" href="/resume.pdf" download="tlykov-cv"
+                <motion.a className="download-btn" href={file} download="tlykov-cv"
                     initial={{opacity: 0}} animate={{opacity: 1}} transition={{opacity: {duration: 1, delay: 0.5}, scale: {type: 'spring', bounce: 0.6}}}
                     whileHover={{scale: 1.05, color: '#df883b', borderColor: '#df883b'}} whileTap={{scale: 0.95}}>
                     Download CV
