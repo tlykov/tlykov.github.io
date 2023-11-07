0cfd19d9164d924b912f532292d5fd543c325b6b
tlykov <tim_lykov@sfu.ca>
2023-11-07 00:26:58 -0800

    refactor btn animation

--- resume-app/src/pages/Home.tsx
+++ resume-app/src/pages/Home.tsx
@@ -1,8 +1,50 @@
-import { motion } from "framer-motion";
+import { motion, Variants } from "framer-motion";
 import pic from '../assets/extra/bg-picture-wide.png?url'
 import file from '../assets/files/resume.pdf?url'
 import '../assets/styles/Home.css';
 import { SideWindow, About } from "../components";
+
+const btnVariants: Variants = {
+    hidden: {
+        opacity: 0,
+        scale: 0.5
+    },
+    show: {
+        opacity: 1,
+        scale: 1,
+        transition: {
+            opacity: {
+                duration: 1, 
+                delay: 0.3
+            },
+            scale: {
+                type: 'spring',
+                duration: 1.4,
+                delay: 0
+            }
+        }
+    },
+    hover: {
+        scale: 1.05,
+        color: '#df883b', 
+        borderColor: '#df883b',
+        transition: {
+            scale: {
+                type: 'spring',
+                bounce: 0.6,
+            }
+        }
+    },
+    tap: {
+        scale: 0.95,
+        transition : {
+            scale: {
+                type: 'spring',
+                bounce: 0.6,
+            }
+        }
+    }
+}
 
 function Home() {
     return (
@@ -11,8 +53,7 @@ function Home() {
             <div className="main-content">
                 <h1 id="subtitle">Software Developer</h1>
                 <motion.a className="download-btn" href={file} download="tlykov-cv"
-                    initial={{opacity: 0}} animate={{opacity: 1}} transition={{opacity: {duration: 1, delay: 0.5}, scale: {type: 'spring', bounce: 0.6}}}
-                    whileHover={{scale: 1.05, color: '#df883b', borderColor: '#df883b'}} whileTap={{scale: 0.95}}>
+                    variants={btnVariants} initial={'hidden'} animate={'show'} whileHover={'hover'} whileTap={'tap'}>
                     Download CV
                 </motion.a>
                 <img id="bg-pic" src={pic}></img>
