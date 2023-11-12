eb5dfcaddda13e536483ac66e82e520b9ea8b2a6
tlykov <tim_lykov@sfu.ca>
2023-11-12 15:57:32 -0800

    project section animation

--- resume-app/src/components/Projects.tsx
+++ resume-app/src/components/Projects.tsx
@@ -1,28 +1,45 @@
 import { useRef } from 'react';
-//import { motion, Variants } from "framer-motion";
+import { motion, Variants } from "framer-motion";
 import '../assets/styles/Projects.css';
 import { ProjectList } from '../utils/ProjectList';
 
 export var projectsRef:any;
 
+const projectVariants: Variants = {
+    offscreen: {
+        x: 80,
+        opacity: 0
+      },
+      onscreen: {
+        x: 0,
+        opacity: 1,
+        transition: {
+          type: "spring",
+          bounce: 0.4,
+          duration: 0.8,
+          delay: 0.2,
+        }
+      }
+}
+
 function Projects() {
     projectsRef = useRef(null);
 
     return (
-        <div id="projects-div" ref={projectsRef}>
+        <motion.div id="projects-div" ref={projectsRef} initial="offscreen" whileInView="onscreen" viewport={{once: true, amount: 0.6}}> 
             <h2>Projects</h2>
             <div id="project-list">
                 {ProjectList.map( (project, i) => 
-                    <div key={i} className={`project ${project.inProgress ? "inProgress" : ""}`}>
+                    <motion.div key={i} className={`project ${project.inProgress ? "inProgress" : ""}`} variants={projectVariants}>
                         <div className='project-top'>
                             <p className="project-title">{project.title}</p>
                             <p className="project-desc">{project.description}</p>
                         </div>
                         <a className="project-link" href={project.link}>Link</a>
-                    </div>
+                    </motion.div>
                 )}
             </div>
-        </div>
+        </motion.div>
     );
 };
 
